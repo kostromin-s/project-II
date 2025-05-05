7928a00cdea8305a085369e88f2c598a21c2cca1
kostromin-s <namly6702@gmail.com>
2025-05-06 00:00:00 +0700

    conversationModel.js utils

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.ADMIN_EMAIL,
+    pass: "bspq pmwf mhuo hawl",
+  },
+});
+
+const sendEmail = async (to, subject, htmlContent) => {
+  const mailOptions = {
+    from: `"My App" <${process.env.ADMIN_EMAIL}>`,
+    to,
+    subject,
+    html: htmlContent,
+  };
+
+  const info = await transporter.sendMail(mailOptions);
+  return info;
+};
+export { sendEmail };
